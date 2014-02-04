81cad7b89682ff90109c10f280e7c429a4a0af92
Dave Miller <pair+dave@pivotallabs.com>
2014-02-03 18:14:36 -0800

    The first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Doogle::Application.config.secret_token = 'e422aedbef821c676bd548a8b7592d71f6404a7aa3c0955a89111bc58e3223b0e8834b0565a6fad8dfb3a219f9a81348417a900ef2f7ad145dc433b6e7cf799a'
